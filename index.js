9f320ba3e89066c35e10ce3c64bf90870b8834f0
Lewis <lewismunyambu@gmail.com>
2022-08-22 16:38:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this 
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+function writeCards(names,ceremony){
+let message = []
+for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${ceremony} gift!`);
+  }
+  return message;
+
+}
+
+function countDown(number){
+    let i = number;
+    while(i>-1){
+        console.log(i--);
+    }
+}countDown(10);
